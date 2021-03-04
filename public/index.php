b60a5c8b275a751128019c6678f2480f015a9ffc
qnguyen1288 <79248959+qnguyen1288@users.noreply.github.com>
2021-03-04 10:12:41 +0000

    Update index.php

--- public/index.php
+++ public/index.php
@@ -21,27 +21,6 @@ $(function() {
 });
 </script>
 
-<script>
-$(function() {
-    $('#OD,#ODF,#other').hide(); 
-    $('#roadmap').change(function(){
-        if($('#roadmap').val() == 'Oxford Discover') {
-            $('#OD').show();
-            $('#ODF').hide(); 
-            $('#other').hide(); 
-        } else if ($('#roadmap').val() == 'Oxford Discover Futures') {
-            $('#OD').hide();
-            $('#ODF').show(); 
-            $('#other').hide(); 
-        } else {
-            $('#OD').hide();
-            $('#ODF').hide(); 
-            $('#other').show(); 
-        }
-    });
-});
-</script>
-
 <style>
 .center {
   text-align: center;
@@ -86,45 +65,6 @@ Lộ trình:
 
 <title>Static Dropdown List</title>  
 
-<div class="giaotrinh" id ="OD">
-Mục tiêu:  
-<select name="muctieu" style="margin-left:57px; width:153px;">  
-  <option value="Select">Select</option>}  
-  <option value="3">A1-</option>  
-  <option value="5">A1</option>  
-  <option value="7">A1+</option>  
-  <option value="9">A2-</option>  
-  <option value="11">A2</option>  
-  <option value="13">A2+</option>  
-  <option value="15">B1-</option>  
-  <option value="17">B1</option>  
-  <option value="19">B1+</option>  
-  <option value="21">B2-</option>  
-  <option value="23">B2</option>  
-  <option value="25">B2+</option>  
-</select>  
-</div> 
-<div class="giaotrinh" id ="ODF">
-Mục tiêu:  
-<select name="muctieu" style="margin-left:57px; width:153px;">  
-  <option value="Select">Select</option>}  
-  <option value="9">A2-</option>  
-  <option value="11">A2</option>  
-  <option value="13">A2+</option>  
-  <option value="15">B1-</option>  
-  <option value="17">B1</option>  
-  <option value="19">B1+</option>  
-  <option value="21">B2-</option>  
-  <option value="23">B2</option>  
-  <option value="25">B2+</option>  
-  <option value="27">C1 Ielts 7.0 - Toefl 94-101</option>  
-  <option value="29">C1 Ielts 7.5 - Toefl 102-109</option>  
-  <option value="31">C1+ Ielts 8.0 - Toefl 110-114</option>  
-  <option value="33">C2- Ielts8.0 - Toefl 110-114</option>  
-  <option value="35">C2 Ielts8.5 - Toefl 115-117</option>  
-  <option value="37">C2+ Ielts9.0 - Toefl 118-120</option>  
-</select>  
-</div> 
 <div class="giaotrinh" id ="other">
 Mục tiêu:  
 <select name="muctieu" style="margin-left:57px; width:153px;">  
